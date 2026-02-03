ef967dd84a0fe7d83cd842a20703b1281ee88b87
azylonn <azylonn@pm.me>
2026-02-03 17:19:52 +0100

    implemented the readme stuff (void the auth thingy)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import {CallbackComponent} from "./components/callback/callback";
+import {EmployeeListComponent} from "./components/employee-list/employee-list.component";
+import {authGuard} from "./guards/auth.guard";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  //{ path: '', component: HomeComponent },
+  { path: 'callback', component: CallbackComponent },
+  { path: 'employees', component: EmployeeListComponent, canActivate: [authGuard] },
+  { path: '**', redirectTo: '' }
+];
