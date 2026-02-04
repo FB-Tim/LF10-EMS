e2d59062e64b15c5c37917cd1345c661bfc08c1c
azylonn <azylonn@pm.me>
2026-02-04 11:50:26 +0100

    fixed components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,11 @@
 import { Routes } from '@angular/router';
+import {HomeComponent} from "./components/home/home.component";
 import {CallbackComponent} from "./components/callback/callback";
 import {EmployeeListComponent} from "./components/employee-list/employee-list.component";
 import {authGuard} from "./guards/auth.guard";
 
 export const routes: Routes = [
-  //{ path: '', component: HomeComponent },
+  { path: '', component: HomeComponent },
   { path: 'callback', component: CallbackComponent },
   { path: 'employees', component: EmployeeListComponent, canActivate: [authGuard] },
   { path: '**', redirectTo: '' }
--- src/app/components/home/home.component.spec.ts
+++ src/app/components/home/home.component.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { Callback } from './callback';
+import { HomeComponent } from './home.component';
 
-describe('Callback', () => {
-  let component: Callback;
-  let fixture: ComponentFixture<Callback>;
+describe('HomeComponent', () => {
+  let component: HomeComponent;
+  let fixture: ComponentFixture<HomeComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [Callback]
+      imports: [HomeComponent]
     })
     .compileComponents();
 
-    fixture = TestBed.createComponent(Callback);
+    fixture = TestBed.createComponent(HomeComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
